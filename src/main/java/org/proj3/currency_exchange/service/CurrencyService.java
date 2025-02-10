52ddbcb3a345a781904a3700f4475b30a9535bd6
Dmitriy Kolesnik <13na18@gmail.com>
2025-02-10 17:13:25 +0200

    feat: get specific currency

--- src/main/java/org/proj3/currency_exchange/service/CurrencyService.java
+++ src/main/java/org/proj3/currency_exchange/service/CurrencyService.java
@@ -1,12 +1,12 @@
 package org.proj3.currency_exchange.service;
 
 import org.proj3.currency_exchange.dao.CurrencyDao;
-import org.proj3.currency_exchange.dto.CurrencyRequestDto;
 import org.proj3.currency_exchange.dto.CurrencyResponseDto;
 import org.proj3.currency_exchange.entity.CurrencyEntity;
 import org.proj3.currency_exchange.mapper.CurrencyMapper;
 
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,11 +30,16 @@ public class CurrencyService {
 
     public Optional<CurrencyResponseDto> findByCode(String code) {
         Optional<CurrencyResponseDto> dto = Optional.empty();
-        Optional<CurrencyEntity> optionalCurrency = currencyDao.findByCode(code);
+        Optional<CurrencyEntity> optionalCurrency = Optional.empty();
+
+        if (isCurrencyCodeExist(code)) {
+            optionalCurrency = currencyDao.findByCode(code);
+        }
 
         if (optionalCurrency.isPresent()) {
             CurrencyEntity entity = optionalCurrency.get();
-            dto = Optional.of(mapper.toDto(entity));
+            CurrencyResponseDto responseDto = mapper.toDto(entity);
+            dto = Optional.of(responseDto);
         }
         return dto;
     }
@@ -46,5 +51,10 @@ public class CurrencyService {
 
     public static CurrencyService getInstance() {
         return currencyService;
+    }
+
+    private boolean isCurrencyCodeExist(String currencyCode) {
+        Currency currency = Currency.getInstance(currencyCode);
+        return currency != null;
     }
 }
