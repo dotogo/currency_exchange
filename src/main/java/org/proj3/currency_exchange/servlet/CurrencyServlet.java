6ca380f4a47e89df1ee9b72813677637b03a0deb
Dmitriy Kolesnik <13na18@gmail.com>
2025-03-13 19:07:28 +0200

    refactor: doGet() in CurrencyServlet

--- src/main/java/org/proj3/currency_exchange/servlet/CurrencyServlet.java
+++ src/main/java/org/proj3/currency_exchange/servlet/CurrencyServlet.java
@@ -1,10 +1,10 @@
 package org.proj3.currency_exchange.servlet;
 
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.proj3.currency_exchange.dto.CurrencyResponseDto;
+import org.proj3.currency_exchange.exception.CurrencyServiceException;
 import org.proj3.currency_exchange.service.CurrencyService;
 import org.proj3.currency_exchange.util.JsonUtill;
 
@@ -12,7 +12,10 @@ import java.io.IOException;
 import java.util.Optional;
 
 @WebServlet("/currency/*")
-public class CurrencyServlet extends HttpServlet {
+public class CurrencyServlet extends BaseServlet {
+    private static final String CURRENCY_CODE_MISSING = "The currency code is missing in the request.";
+    private static final String CURRENCY_NOT_FOUND = "Currency not found.";
+
     CurrencyService currencyService = CurrencyService.getInstance();
 
     @Override
@@ -20,7 +23,7 @@ public class CurrencyServlet extends HttpServlet {
         String unverifiedCurrencyCode = req.getPathInfo();
 
         if (unverifiedCurrencyCode == null || unverifiedCurrencyCode.equals("/")) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "The currency code is missing in the request.");
+            sendErrorResponse(resp, HttpServletResponse.SC_BAD_REQUEST, CURRENCY_CODE_MISSING);
             return;
         }
 
@@ -29,15 +32,14 @@ public class CurrencyServlet extends HttpServlet {
             if (dtoOptional.isPresent()) {
                 CurrencyResponseDto responseDto = dtoOptional.get();
                 String json = JsonUtill.toJson(responseDto);
+
                 resp.setStatus(HttpServletResponse.SC_OK);
                 resp.getWriter().write(json);
             } else {
-                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                resp.getWriter().write("Currency not found.");
+                sendErrorResponse(resp, HttpServletResponse.SC_NOT_FOUND, CURRENCY_NOT_FOUND);
             }
-        } catch (Exception e) {
-            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            resp.getWriter().write("Internal server error: " + e.getMessage());
+        } catch (CurrencyServiceException e) {
+            sendErrorResponse(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
 }
